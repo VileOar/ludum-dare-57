f8031a08d7a536d642f77acea9d1a030271e246c
Francisco Esteves <47716466+Steeeves@users.noreply.github.com>
2025-04-06 16:40:17 +0200

    Add a bunch of audio effects and a test music

--- src/audio_controller/audio_controller.gd
+++ src/audio_controller/audio_controller.gd
@@ -4,10 +4,15 @@ extends Node2D
 @onready var _squeak1: AudioStreamPlayer = $Squeak1 
 @onready var _squeak2: AudioStreamPlayer = $Squeak2 
 @onready var _squeak3: AudioStreamPlayer = $Squeak3
+@onready var _music: AudioStreamPlayer = $Music
 var _squeaks: Array[AudioStreamPlayer] = []
 
 func _ready() -> void:
 	_squeaks = [_squeak1, _squeak2, _squeak3]
+	play_music()
+
+func play_music() -> void:
+	_music.play()
 
 # podem usar isto para quando o player levar damage
 func play_squeak(random_pitch: bool = true) -> void:
