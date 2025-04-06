e5471dc7db94dbafef25d9b3d914ac7338d41916
VileOar <37459155+VileOar@users.noreply.github.com>
2025-04-06 22:38:08 +0100

    tweaks

--- main_menu.gd
+++ main_menu.gd
@@ -1,18 +1,23 @@
 extends Control
 
 const PLAY_SCENE: String = "res://levels/main_level.tscn"
+
+@onready var _menu_margin_container: MarginContainer = %MenuMarginContainer
+@onready var _loading_margin_container: MarginContainer = %LoadingMarginContainer
+@onready var _loading_bar: ProgressBar = $LoadingMarginContainer/VBoxContainer/LoadingBar
+
 var new_scene: Node = null
 
 func _on_play_pressed() -> void:
-	$MenuMarginContainer.hide()
-	$LoadingMarginContainer.show()
+	_menu_margin_container.hide()
+	_loading_margin_container.show()
 	new_scene = load(PLAY_SCENE).instantiate()
 	get_tree().root.add_child(new_scene)
 	new_scene.visible = false
 	
 func _process(delta: float) -> void:
 	if Global.world_map_tiles:
-		$LoadingMarginContainer/VBoxContainer/ProgressBar.value = Global.world_map_tiles.load_percent
+		_loading_bar.value = Global.world_map_tiles.load_percent
 		if Global.world_map_tiles.are_tiles_generated and new_scene:
 			var current = get_tree().current_scene
 			if current:
