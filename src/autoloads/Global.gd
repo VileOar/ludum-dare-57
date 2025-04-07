af87d55e2473c9b0e752278a408a8824b9282c46
Miguel Monteiro <66484852+exodusgd@users.noreply.github.com>
2025-04-07 20:04:22 +0100

    Shop upgrades
    - Buying upgrades in the shop now actually applies upgrades to the player
    - Added shopkeeper portrait

--- src/autoloads/Global.gd
+++ src/autoloads/Global.gd
@@ -18,7 +18,25 @@ var max_fuel := 10
 @export var TIME_BETWEEN_ENEMY_SPAWNS : float = 0.15
 
 const CELL_SIZE = 128
-const MAX_MINING_STRENGTH: int = 4
+const MAX_MINING_STRENGTH: int = 3
+
+# Upgrades
+# Health
+const HEALTH_UPGRADE_1: int = 150
+const HEALTH_UPGRADE_2: int = 200
+const HEALTH_UPGRADE_3: int = 300
+# Stamina
+const FUEL_UPGRADE_1: int = 15
+const FUEL_UPGRADE_2: int = 20
+const FUEL_UPGRADE_3: int = 30
+# Claws
+const DRILL_UPGRADE_1: int = 1
+const DRILL_UPGRADE_2: int = 2
+const DRILL_UPGRADE_3: int = 3
+#Scanner
+const SCANNER_UPGRADE_1: int = 2
+const SCANNER_UPGRADE_2: int = 3
+const SCANNER_UPGRADE_3: int = 4
 
 const INTERACTABLE_GROUP: String = "Interactable"
 
@@ -85,6 +103,48 @@ func get_currency() -> int:
 	return _currency
 
 
-func add_upgrade(upgrade_type):
+func add_upgrade(upgrade_type: Upgrades):
 	_current_upgrades[upgrade_type] = 1;
-	
+	match  upgrade_type:
+		Upgrades.HEALTH_1:
+			max_health = HEALTH_UPGRADE_1
+			player_ref.set_health_to_max()
+		
+		Upgrades.HEALTH_2:
+			max_health = HEALTH_UPGRADE_2
+			player_ref.set_health_to_max()
+		
+		Upgrades.HEALTH_3:
+			max_health = HEALTH_UPGRADE_3
+			player_ref.set_health_to_max()
+		
+		Upgrades.FUEL_1:
+			max_fuel = FUEL_UPGRADE_1
+			player_ref.set_fuel_to_max()
+		
+		Upgrades.FUEL_2:
+			max_fuel = FUEL_UPGRADE_2
+			player_ref.set_fuel_to_max()
+		
+		Upgrades.FUEL_3:
+			max_fuel = FUEL_UPGRADE_3
+			player_ref.set_fuel_to_max()
+		
+		Upgrades.DRILL_1:
+			player_ref.set_mining_strength(DRILL_UPGRADE_1)
+		
+		Upgrades.DRILL_2:
+			player_ref.set_mining_strength(DRILL_UPGRADE_2)
+		
+		Upgrades.DRILL_3:
+			player_ref.set_mining_strength(DRILL_UPGRADE_3)
+		
+		Upgrades.SCANNER_1:
+			player_ref.set_radar_level(SCANNER_UPGRADE_1)
+		
+		Upgrades.SCANNER_2:
+			player_ref.set_radar_level(SCANNER_UPGRADE_2)
+		
+		Upgrades.SCANNER_3:
+			player_ref.set_radar_level(SCANNER_UPGRADE_3)
+		
