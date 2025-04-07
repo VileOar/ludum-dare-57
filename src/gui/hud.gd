670ba03e3b0c0da3f2dfda6f052c5dbe26f03141
VileOar <37459155+VileOar@users.noreply.github.com>
2025-04-07 17:22:52 +0100

    post merge fix

--- src/gui/hud.gd
+++ src/gui/hud.gd
@@ -10,7 +10,9 @@ func _ready() -> void:
 	update_stamina_bar(Global.max_fuel)
 
 func update_health_bar(new_health: int) ->void:
+	@warning_ignore("integer_division")
 	_health_bar.value = new_health * 100 / Global.max_health
 
 func update_stamina_bar(new_stamina: int) ->void:
+	@warning_ignore("integer_division")
 	_stamina_bar.value = new_stamina * 100 / Global.max_fuel
