b81380d0104b072e1e06e61a4a7950a87a682a4c
Miguel Monteiro <66484852+exodusgd@users.noreply.github.com>
2026-03-10 23:16:57 +0000

    Credits and How To Play update
    - Updated the look of the credits and how to play menus.

--- src/mainmenu/main_menu.gd
+++ src/mainmenu/main_menu.gd
@@ -1,30 +1,33 @@
 extends Control
 
-@onready var _main_menu = $MainMenu
+@onready var _main_menu = $TitleMenu
+@onready var _credits = $Credits
+@onready var _how_to = $HowTo
+
+func _ready() -> void:
+	# ensure default visibility
+	_main_menu.show()
+	_credits.hide()
+	_how_to.hide()
 
 func _on_play_pressed() -> void:
 	Global.deferred_change_scene(Global.level_scene)
 
-
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 
-
 func _on_credits_pressed() -> void:
 	_main_menu.hide()
-	$Credits.visible = true
+	_credits.show()
 
-
-func _on_back_pressed() -> void:
-	$Credits.visible = false
+func _on_credits_back_pressed() -> void:
+	_credits.hide()
 	_main_menu.show()
-
 
 func _on_how_to_pressed() -> void:
 	_main_menu.hide()
-	$HowTo.visible = true
+	_how_to.show()
 
-
-func _on_back_how_to_pressed() -> void:
-	$HowTo.visible = false
+func _on_how_to_back_pressed() -> void:
+	_how_to.hide()
 	_main_menu.show()
