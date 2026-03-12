770913269fe2a37c98aaef8f94b6dd667a2d7714
Miguel Monteiro <66484852+exodusgd@users.noreply.github.com>
2026-03-12 20:51:48 +0000

    End Menu update
    Updated the look of the end menu.

--- src/gui/end_menu.gd
+++ src/gui/end_menu.gd
@@ -2,12 +2,11 @@ extends Control
 
 @onready var status_label: Label = %StatusLabel
 
-
 func _ready():
 	if Global.end_state:
 		status_label.text = "You made it!"
 	else:
-		status_label.text = "You didn't make it!"
+		status_label.text = "You didn't make it..."
 
 
 func _on_button_pressed():
